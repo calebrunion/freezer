126ab0ec4dab1348307879e2a5945beabae751f7
Caleb Runion <calebrunion@icloud.com>
2021-03-20 15:30:25 -0400

    Import "Head"

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Notes</title>
+<link rel="stylesheet" href="../../css/style.css">
+<link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+<link rel="icon" href="../../icon.jpg" sizes="32x32">
+<link rel="icon" href="../../icon.jpg" sizes="192x192">
+<link rel="apple-touch-icon" href="../../icon.jpg">
+
+
+<main>
+    {{ $slot }}
+</main>
