df9dab0cb6f51ce8467ebda85379d7c510d667ba
Caleb Runion <calebrunion@icloud.com>
2021-03-31 13:14:32 -0400

    Create app.layout file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<script src="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
+<script>
+    WebFont.load({
+        google: {
+            families: ['Montserrat:400,700']
+        }
+    });
+</script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Notes</title>
+<link rel="stylesheet" href="{{ asset('css/app.min.css') }}">
+@livewireStyles
+<script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+
+<body class="flex flex-col font-sans dark:bg-gray-800 dark:text-white">
+    <main class="">
+        {{ $slot }}
+    </main>
+
+    @livewireScripts
+</body>
